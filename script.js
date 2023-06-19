1d03ec4e88691bfba3e5170769053d4e1bff7883
Anshul Dewangan <101018041+Anshul-04@users.noreply.github.com>
2023-06-19 23:47:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function decimalToBinary(decimal) {
-  return decimal.toString(2);
+  let binary = "";
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
 }
-
-
 
