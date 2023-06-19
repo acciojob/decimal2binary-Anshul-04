fc9bcd95b3437401544e2de5143bf4db6750b271
Anshul Dewangan <101018041+Anshul-04@users.noreply.github.com>
2023-06-19 23:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+// function decimalToBinary(decimal) {
+//   let binary = "";
+//   while (decimal > 0) {
+//     binary = (decimal % 2) + binary;
+//     decimal = Math.floor(decimal / 2);
+//   }
+//   return binary;
+// }
 function decimalToBinary(decimal) {
-  let binary = "";
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-  return binary;
+  return decimal.toString(2);
 }
+console.log(decimalToBinary(7)); // Output: 111
+console.log(decimalToBinary(10)); // Output: 1010
+console.log(decimalToBinary(33));
+
 module.exports = decimalToBinary;
