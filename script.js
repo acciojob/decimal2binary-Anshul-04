9d5f6e535091953641d3e330f933ead927027aef
Anshul Dewangan <101018041+Anshul-04@users.noreply.github.com>
2023-06-19 23:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,8 +9,5 @@
 function decimalToBinary(decimal) {
   return decimal.toString(2);
 }
-console.log(decimalToBinary(7)); // Output: 111
-console.log(decimalToBinary(10)); // Output: 1010
-console.log(decimalToBinary(33));
 
 module.exports = decimalToBinary;
